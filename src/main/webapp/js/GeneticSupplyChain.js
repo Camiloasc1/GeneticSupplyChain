47ee92bc255731333b4d2d4070157560b59a32b5
caasanchezcu <caasanchezcu@unal.edu.co>
2016-11-05 19:52:40 -0500

    Index and Angular App.

---
+++ src/main/webapp/js/GeneticSupplyChain.js
@@ -0,0 +1,22 @@
+"use strict";
+var app = angular.module('GeneticSupplyChain', ['ngRoute']);
+
+app.controller('NavigationController', ['$scope', '$http', function ($scope, $http) {
+}]);
+
+app.controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+}]);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeController'
+        })
+
+        .otherwise({redirectTo: '/'});
+
+    //html5mode causes several issues when the front end is embedded with the web service.
+    //$locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+}]);
